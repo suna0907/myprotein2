2f663b3665b708b6a94cb63bc8e2f4a5e7d7f008
HIRAGA <fuya_hiraga@totec.co.jp>
2023-06-01 14:13:40 +0900

    7.10photo/urls.py

--- photo/urls.py
+++ photo/urls.py
@@ -14,10 +14,21 @@ urlpatterns = [
          views.CategoryView.as_view(),
          name = 'photos_cat'
          ),
-         
+
     path('user-list/<int:user>',
          views.UserView.as_view(),
          name = 'user_list'
          ),
 
+    path('photo-detail/<int:pk>',
+         views.DetailView.as_view(),
+         name = 'photo_detail'
+         ),
+
+    path('mypage/', views.MypageView.as_view(), name = 'mypage'),
+
+    path('photo/<int:pk>/delete/',
+         views.PhotoDeleteView.as_view(),
+         name = 'photo_delete'
+         ),
 ]
