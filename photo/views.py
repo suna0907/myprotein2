45e284d3ff7da15d2c8880726c43e808694ae532
kawaguchi <kota_kawaguchi@totec.co.jp>
2023-06-01 10:24:22 +0900

    7-4 ファイル6つ p442-451

--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
-#djang.views.geneticからTemplateViewをインポート
+
+# Create your views here.
 from django.views.generic import TemplateView
 
+from django.views.generic import CreateView
+
+from django.urls import reverse_lazy
+
+from .forms import PhotoPostForm
+
+from django.utils.decorators import method_decorator
+
+from django.contrib.auth.decorators import login_required
+
 class IndexView(TemplateView):
-    #トップページのビュー
-#index.htmlをレンダリングする
+
     template_name = 'index.html'
-# Create your views here.
+
+
+@method_decorator(login_required, name='dispatch')
+class CreatePhotoView(CreateView):
+
+
+    form_class = PhotoPostForm
+
+    template_name = "post_photo.html"
+
+    success_url = reverse_lazy('photo:post_done')
+
+    def form_valid(self, form):
+
+
+        postdata = form.save(commit=False)
+
+        postdata.user = self.request.user
+
+        postdata.save()
+
+        return super().form_valid(form)
+    
+class PostSuccessView(TemplateView):
+
+    template_name = 'post_success.html'
