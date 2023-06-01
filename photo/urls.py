f395a7eb287c3c49d965afbf4256fc014185ef0e
HIRAGA <fuya_hiraga@totec.co.jp>
2023-06-01 13:34:55 +0900

    7-6photo/urls.py

--- photo/urls.py
+++ photo/urls.py
@@ -6,4 +6,12 @@ app_name = 'photo'
 urlpatterns = [
 
     path('', views.IndexView.as_view(), name='index'),
+    path('post/', views.CreatePhotoView.as_view(), name = 'post'),
+    path('post_done/',
+         views.PostSuccessView.as_view(),
+         name = 'post_done'),
+    path('photos/<int:category>',
+         views.CategoryView.as_view(),
+         name = 'photos_cat'
+         ),
 ]
